0723dd29d07c7ed15ff3756ec5fee42231d5c950
Creg Bradley <cbradley@daptiv.com>
2013-06-21 10:36:28 -0700

    Added install task to install node modules in client, server

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,16 @@ module.exports = function(grunt) {
 
 	grunt.initConfig({
 		exec: {
+			clientinstall: {
+				cmd: 'npm install',
+				cwd: 'client'
+			},
+			serverinstall: {
+				cmd: 'npm install',
+				cwd: 'server'
+			},
+			
+			
 			client: {
 				cmd: 'grunt --gruntfile client/Gruntfile.js'
 			},
@@ -16,6 +26,11 @@ module.exports = function(grunt) {
 	});
 
 	grunt.loadNpmTasks('grunt-exec');
+	
+	grunt.registerTask('install', [
+		'exec:clientinstall',
+		'exec:serverinstall'
+	]);
 	
 	grunt.registerTask('build', [
 		'exec:clientbuild'
